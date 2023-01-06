f7341e75258f1e736617fdc1e3e6243d3d481f02
bbakercello <bbakercello@gmail.com>
2023-01-06 11:09:37 -0500

    added pagination to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
 import NavigationBar from "../components/NavigationBar";
 import { PrismaClient } from "@prisma/client";
 import Link from "next/link";
+import { useState } from "react";
 const prisma = new PrismaClient();
 import { GetServerSideProps } from "next";
+
+const ITEMS_PER_PAGE = 10;
+
 const IndexPage = ({ feed }) => {
-  console.log(feed);
+  // Declare a state variable for the current page
+  const [page, setPage] = useState(1);
+
+  // Calculate the starting and ending indices of the items to display on the current page
+  const startIndex = (page - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+
+  // Use the slice method to get the items for the current page
+  const pageItems = feed.slice(startIndex, endIndex);
+
   return (
     <>
       <NavigationBar />
-      {feed.map((item) => (
+      {pageItems.map((item) => (
         <div className="flex items-center p-4 border-b border-gray-200 hover:bg-gray-100">
           <Link href={{ pathname: `./posts/${item.id}` }}>
             <img
@@ -23,6 +36,16 @@ const IndexPage = ({ feed }) => {
           </div>
         </div>
       ))}
+      {/* Add pagination controls */}
+      <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+        Previous
+      </button>
+      <button
+        onClick={() => setPage(page + 1)}
+        disabled={page === Math.ceil(feed.length / ITEMS_PER_PAGE)}
+      >
+        Next
+      </button>
     </>
   );
 };
